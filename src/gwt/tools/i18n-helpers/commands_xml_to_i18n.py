0f0206a8783c9470c3127acd3f6046dc6ef67d30
Andrew Scribner <ca.scribner+1@gmail.com>
2021-04-08 11:45:10 -0400

    refactor commands_xml_to_i18n.py to easily separate interface and properties
    Really this makes more sense to be separate scripts or have a more modular entrypoint, but haven't done that yet.

--- src/gwt/tools/i18n-helpers/commands_xml_to_i18n.py
+++ src/gwt/tools/i18n-helpers/commands_xml_to_i18n.py
@@ -3,8 +3,8 @@ import argparse
 from command import xml_to_commands_interface, xml_to_commands_properties
 
 ELEM_PARSERS = {}
-COMMAND_CONSTANTS_DEFAULT_FILENAME = "CommandConstants.java"
-COMMAND_PROPERTIES_DEFAULT_FILENAME = "CommandConstants_en.properties"
+COMMAND_CONSTANTS_DEFAULT_FILENAME = None
+COMMAND_PROPERTIES_DEFAULT_FILENAME = None
 
 
 def parse_args():
@@ -16,18 +16,23 @@ def parse_args():
     parser.add_argument("--command_constant_file",
                         type=str,
                         default=COMMAND_CONSTANTS_DEFAULT_FILENAME,
-                        help=f"Filename to output constants for Commands (default {COMMAND_CONSTANTS_DEFAULT_FILENAME})"
+                        help=f"Filename to output constants for Commands (if omitted, file will not be written)"
                         )
+    parser.add_argument("--command_constant_prefix",
+                        type=str,
+                        default="",
+                        help=f"Text prefix for all default command values in the java interface.  Useful to visually "
+                             f"highlight when a default text statement is used vs a translated one during debugging")
     parser.add_argument("--command_properties_file",
                         type=str,
                         default=COMMAND_PROPERTIES_DEFAULT_FILENAME,
-                        help=f"Filename to output properties for Commands (default {COMMAND_PROPERTIES_DEFAULT_FILENAME})"
+                        help=f"Filename to output properties for Commands (if omitted, file will not be written)"
                         )
-    parser.add_argument("--prefix",
+    parser.add_argument("--command_properties_prefix",
                         type=str,
                         default="",
-                        help=f"Text prefix for all default command values.  Useful to highlight a default text "
-                             f"statement from a translated English one")
+                        help=f"Text prefix for all entries in the emitted properties file.  Useful to visually "
+                             f"highlight when a string from this property file is used during debugging")
     parser.add_argument("--packages", "--package",
                         default=[],
                         action='append',
@@ -43,13 +48,15 @@ if __name__ == "__main__":
     tree = ET.parse(args.xmlfile)
     root = tree.getroot()
 
-    constants = xml_to_commands_interface(root, args.prefix)
-    for package in args.packages:
-        constants.add_package(package)
-    constants.write(args.command_constant_file)
+    if args.command_constant_file is not None:
+        constants = xml_to_commands_interface(root, args.command_constant_prefix)
+        for package in args.packages:
+            constants.add_package(package)
+        constants.write(args.command_constant_file)
 
-    properties = xml_to_commands_properties(root, args.prefix)
-    properties.write(args.command_properties_file)
+    if args.command_properties_file is not None:
+        properties = xml_to_commands_properties(root, args.command_properties_prefix)
+        properties.write(args.command_properties_file)
 
 #
 #     # for elem in tree.iter():
